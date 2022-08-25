2935f46b5abf0a3270f0b6a68d2727cd57a6cd8c
Endor 8 <66875464+Endor-8@users.noreply.github.com>
2022-08-25 20:36:21 +0200

    Update build script to Kotlin DSL

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    `kotlin-dsl`
+}
+
+repositories {
+    maven("https://files.minecraftforge.net/maven") {
+        name = "Minecraft Forge"
+    }
+    maven("https://repo.spongepowered.org/maven") {
+        name = "Sponge Powered"
+    }
+    mavenCentral()
+    gradlePluginPortal()
+}
+
+dependencies {
+    implementation("net.minecraftforge.gradle:ForgeGradle:5.1.+") {
+        isChanging = true
+    }
+    implementation("org.spongepowered:mixingradle:0.7.+")
+}
