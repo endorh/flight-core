ae95a32f72beae825489c9e5b8679d0709d5f7b0
Endor H <86890810+endorh@users.noreply.github.com>
2022-09-05 13:10:57 +0200

    🔧 Add GitHub Packages repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ plugins {
 // Mod info
 val modId = "flightcore"
 val modGroup = "endorh.flightcore"
+val githubRepo = "endorh/flightcore"
 val modVersion = "0.5.5"
 val mcVersion = "1.19.2"
 val forge = "43.1.1"
@@ -281,6 +282,14 @@ artifacts {
 
 publishing {
 	repositories {
+		maven("https://maven.pkg.github.com/$githubRepo") {
+			name = "GitHubPackages"
+			credentials {
+				username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+				password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+			}
+		}
+		
 		maven(rootProject.projectDir.parentFile.resolve("maven")) {
 			name = "LocalMods"
 		}
